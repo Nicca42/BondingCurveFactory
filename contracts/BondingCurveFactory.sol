6a3f005ba5c3425090fc359b210ab1cb313e911f
Veronica <veronica.21.coutts@gmail.com>
2020-05-06 15:38:27 +0200

    Adding basic functions

--- contracts/BondingCurveFactory.sol
+++ contracts/BondingCurveFactory.sol
@@ -2,11 +2,32 @@ pragma solidity 0.6.6;
 
 contract BondingCurveFactory {
     
-    function createBondingCurve(
-
+    function createBasicBondingCurve(
+        uint8 _curveType
     )
         public
     {
         
     }
+
+    function addCurveType(
+        string _curveName,
+        uint256[] _curvParameters,
+        address _curveAddress
+    )
+        public
+        returns(uint8)
+    {
+        
+    }
+
+    function _AddEnsName(string _ensName) internal {
+
+    }
+
+    function _CreateLinearCurve(uint256 _a) internal {
+
+    }
+
+
 }
