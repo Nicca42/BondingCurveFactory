94fd4683231989a84ca366989c6c510b834603b5
Veronica <veronica.21.coutts@gmail.com>
2020-05-11 16:28:57 +0200

    Adding etherlime

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,15 +1,39 @@
 pragma solidity 0.6.6;
 
 import "./I_Token.sol";
+import "./I_Curve.sol";
 
 contract Token is I_Token {
+    uint256 public totalSupply;
+    uint256 public currentSupply;
+    uint256 public a;
+    uint256 public b;
+    uint256 public c;
+    I_Curve public curveInstance;
 
-    constructor(uint256 _totalSupply) public {
-
+    constructor(
+        address _curveInstance,
+        uint256 _totalSupply,
+        uint256 _a,
+        uint256 _b,
+        uint256 _c
+    ) 
+        public 
+    {
+        curveInstance = I_Curve(_curveInstance);
+        totalSupply = _totalSupply;
+        currentSupply = 0;
+        a = _a;
+        b = _b;
+        c = _c;
     }
 
-    function getSupply() override(I_Token) external view returns(uint256) {
+    function getBuyCost(uint256 _tokens) public view returns(uint256) {
+        return curveInstance.getBuyPrice(_tokens);
+    } 
 
+    function getSupply() override(I_Token) external view returns(uint256) {
+        return currentSupply;
     }
     
     function getCurve() override(I_Token) external view returns (
@@ -17,6 +41,6 @@ contract Token is I_Token {
         uint256,
         uint256
     ) {
-        
+        return (a, b, c);
     }
 }
--- test/Curve.test.js
+++ test/Curve.test.js
@@ -1,15 +1,42 @@
-const Curve = artifacts.require("Curve");
+const etherlime = require('etherlime-lib');
+const ethers = require('ethers');
+const BigNumber = require('bignumber.js');
 
-contract("💰 Curve Tests", async accounts => {
 
-    it("💰 Get token price", async () => {
-        let instance = await MetaCoin.deployed();
-        let balance = await instance.getBalance.call(accounts[0]);
+let TokenAbi = require('../build/Token.json');
+let CurveAbi = require('../build/Curve.json');
 
-        assert.equal(balance.valueOf(), 10000);
+describe("💰 Curve Tests", async () => {
+    let insecureDeployer = accounts[0];
+    
+    let tokenInstance;
+    let curveInstance;
+
+    beforeEach('', async () => {
+        deployer = new etherlime.EtherlimeGanacheDeployer(insecureDeployer.secretKey);
+        
+        curveInstance = await deployer.deploy(
+            CurveAbi,
+            false
+        );
+
+        tokenInstance = await deployer.deploy(
+            TokenAbi,
+            false,
+            curveInstance.contract.address,
+            100,
+            5,
+            4,
+            3
+        );
     });
 
     it("💰 Get token price", async () => {
+        let buyPrice = await tokenInstance.getBuyCost(10);
+        console.log(buyPrice.toString())
+    });
 
+    it("💰 Get token price", async () => {
+        
     });
 });
