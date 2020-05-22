37911a1e93964b418412ea73a713504fa272df4c
Veronica <veronica.21.coutts@gmail.com>
2020-05-22 15:11:14 +0200

    Market transition documentation complete

--- contracts/I_MarketTransition.sol
+++ contracts/I_MarketTransition.sol
@@ -3,7 +3,7 @@ pragma solidity 0.6.6;
 interface I_MarketTransition {
 
     function transition() external;
-    function getTokensToMint() external returns(uint256 tokensToMint);
+    function getTokensToMint() external returns(uint256);
     function getRouterAddress() external view returns(address);
     function getTransitionInfo(address _token) external view returns(uint, uint, uint);
     function getMonthsFutureTimestamp(uint256 _months) external view returns(uint256);
--- contracts/MarketTransition.sol
+++ contracts/MarketTransition.sol
@@ -3,44 +3,39 @@ pragma solidity 0.6.6;
 import "./IUniswapV2Router01.sol";
 import "./I_Token.sol";
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
-import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "./BokkyPooBahsDateTimeLibrary.sol";
 
-contract MarketTransition is ERC20 {
+contract MarketTransition {
     using BokkyPooBahsDateTimeLibrary for uint256;
-    
+    // Creates an instance of the uniswap router
     IUniswapV2Router01 public routerInstance;
-    bool public check;
 
+    bool public check;
     mapping(address => uint[3]) public transitionInfo;
 
     event transitionToFreeMarket(uint amountA, uint amountB, uint liquidity);
-    event wtf(uint tokens, uint collateral, address sender);
 
-    constructor(
-        address _uniswapRouter
-    )
-        ERC20(
-            "test", "TST"
-        )
-        public 
-    {
+    constructor(address _uniswapRouter) public {
         routerInstance = IUniswapV2Router01(_uniswapRouter);
     }
 
     function transition() public {
+        // Creates an instance of the token sender.
         I_Token tokenInstance = I_Token(msg.sender);
-        IERC20 collateralInstance = IERC20(tokenInstance.getCollateralInstance());
+        // Creates an instance of the collateral token of the token.
+        IERC20 collateralInstance = IERC20(
+            tokenInstance.getCollateralInstance()
+        );
         // // This gets the price of the next token in collateral
         uint256 currentPrice = transitionInfo[msg.sender][0];
         uint256 tokensToMint = transitionInfo[msg.sender][1];
         uint256 collateralInToken = transitionInfo[msg.sender][2];
 
-        emit wtf(tokensToMint, collateralInToken, msg.sender);
-
-        //TODO Checks if a pair is already created 
+        // TODO Checks if a pair is already created 
         // TODO if there is then the min A & B need to be sliders not set
         {
+            // Approves the uniswap router as a spender for the collateral and
+            // tokens.
             require(
                 collateralInstance.approve(
                     address(routerInstance),
@@ -48,7 +43,6 @@ contract MarketTransition is ERC20 {
                 ),
                 "Transfer of collateral failed"
             );
-
             require(
                 tokenInstance.approve(
                     address(routerInstance),
@@ -56,8 +50,12 @@ contract MarketTransition is ERC20 {
                 ),
                 "Transfer of minted tokens failed"
             );
-
-            (uint amountA, uint amountB, uint liquidity) = routerInstance.addLiquidity(
+            // Creates and adds liquidity for the pair on uniswap. 
+            (
+                uint amountA, 
+                uint amountB, 
+                uint liquidity
+            ) = routerInstance.addLiquidity(
                 address(tokenInstance),
                 address(collateralInstance),
                 tokensToMint,
@@ -70,29 +68,64 @@ contract MarketTransition is ERC20 {
 
             emit transitionToFreeMarket(amountA, amountB, liquidity);
         }
-
+        // Sets the token to transitioned.
         I_Token(msg.sender).setTransition();
     }
 
-    function getTokensToMint() public returns(uint256 tokensToMint) {
+    /**
+      * @notice This functions stores the information around the prices for the
+      *         token as when this information was not stored the calculations
+      *         would inherintly be different as there would be new tokens 
+      *         minted between this function and the transition function 
+      *         iteself.
+      * @return uint256: The number of tokens the token will need to mint
+      *         for the start price of the uniswap market to be the same as the
+      *         end price of the bonding curve.
+      */
+    function getTokensToMint() public returns(uint256) {
         I_Token tokenInstance = I_Token(msg.sender);
         uint256 currentPrice = tokenInstance.getBuyCost(1);
-
-        IERC20 collateralInstance = IERC20(tokenInstance.getCollateralInstance());
+        // Makes an instance of the collateral token of the token
+        IERC20 collateralInstance = IERC20(
+            tokenInstance.getCollateralInstance()
+        );
+        
         uint256 collateralInToken = collateralInstance.balanceOf(msg.sender);
-
-        tokensToMint = collateralInToken/currentPrice;
+        uint256 tokensToMint = collateralInToken/currentPrice;
 
         transitionInfo[msg.sender][0] = currentPrice;
         transitionInfo[msg.sender][1] = tokensToMint;
         transitionInfo[msg.sender][2] = collateralInToken;
+
+        return tokensToMint;
     }
 
+    /**
+      * @return address: The address of the uniswap router. 
+      */
     function getRouterAddress() public view returns(address) {
         return address(routerInstance);
     }
 
-    function getTransitionInfo(address _token) public view returns(uint, uint, uint) {
+    /**
+      * @param  _token: The address of the token.
+      * @return uint: The current price. The price of the token at transition.
+      *         This should be the start price of the uniswap market. 
+      * @return uint: The tokens minted to the transition contract.
+      * @return uint: The collateral moved out of the bonding curve and into the
+      *         uniswap market.
+      */
+    function getTransitionInfo(
+        address _token
+    ) 
+        public 
+        view 
+        returns(
+            uint, 
+            uint, 
+            uint
+        ) 
+    {
         return (
             transitionInfo[_token][0],
             transitionInfo[_token][1],
@@ -100,7 +133,18 @@ contract MarketTransition is ERC20 {
         );
     }
 
-    function getMonthsFutureTimestamp(uint256 _months) public view returns(uint256) {
+    /**
+      * @notice I needed a time calaculator somewhere, so I added it in here.
+      * @param  _months: The number of months to be converted into unix time.
+      * @return uint256: The time stamp of the months in unix time.
+      */
+    function getMonthsFutureTimestamp(
+        uint256 _months
+    ) 
+        public 
+        view 
+        returns(uint256) 
+    {
         return now.addMonths(_months);
     }
 }
